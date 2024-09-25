355bb36266511966dec576d733e1f93dcd735d78
Ruslan Tkhorenko <gortakill@gmail.com>
2024-09-25 18:55:02 +0300

    fix

--- client/src/api/socket.api.ts
+++ client/src/api/socket.api.ts
@@ -1,26 +1,28 @@
-import io, { Socket } from "socket.io-client"
-
+import io, { Socket } from 'socket.io-client';
 
 export class SocketApi {
     static socket: null | Socket = null;
 
     static createConnection() {
-        if(!this.socket){
-            this.socket = io('http://localhost:5002/')
+        if (!this.socket) {
+            this.socket = io(
+                'https://dumky-social-server-production.up.railway.app/',
+            );
 
             this.socket.on('connect', () => {
-                console.log('succes connection')
-            })
-    
+                console.log('succes connection');
+            });
+
             this.socket.on('disconnect', () => {
-                console.log('socket disconnected')
-                this.socket = null
-            })
+                console.log('socket disconnected');
+                this.socket = null;
+            });
             this.socket.on('client-path', (message) => {
-                console.log('Received message:', message)});
+                console.log('Received message:', message);
+            });
         }
-        if(this.socket){
-            return
+        if (this.socket) {
+            return;
         }
     }
 }
